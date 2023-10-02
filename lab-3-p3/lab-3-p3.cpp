94f71c51a73a5d80a0421add05bfa787ffae035a
ZarwalD <mdogminec@gmail.com>
2023-10-02 11:19:59 +0700

    Добавьте файлы проекта.

---
+++ lab-3-p3/lab-3-p3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+/**
+ *
+ * @brief Нахождение значения выражения кусочной функции
+ * 
+ * @return 0
+ * 
+**/
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	float a, b, x;
+	std::cout << "Введите а: ";
+	std::cin >> a;
+	std::cout << "Введите b: ";
+	std::cin >> b;
+	std::cout << "Введите x: ";
+	std::cin >> x;
+	float f;
+	if (x < -2.45)
+	{
+		f = b - powf(x, 2) - 1;
+		std::cout << "f(x) = " << f << std::endl;
+	}
+	else if (x >= -2.45 && x < 5)
+	{
+		f = powf(cosf(a * powf(x, 2) + 3), 2);
+		std::cout << "f(x) = " << f << std::endl;
+	}
+	else if (x >= 5)
+	{
+		f = sqrtf(logf(x + a));
+		std::cout << "f(x) = " << f << std::endl;
+	}
+	else
+	{
+		std::cout << "x не входит ни в один заданный диапазон" << std::endl;
+	}
+}
